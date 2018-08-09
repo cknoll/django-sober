04578919360c6970dec0682cb2b8ed0e36abbf4c
Carsten Knoll <carsten.knoll@posteo.de>
2018-08-10 00:14:04 +0200

    show the first content from database with a view

--- sober_app/admin.py
+++ sober_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Brick
+
+admin.site.register(Brick)
+
 # Register your models here.
--- sober_app/views.py
+++ sober_app/views.py
@@ -1,7 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Brick
 
 
 def index(request):
     return HttpResponse("Hello, world. This is sober.")
 
 
+def renderbrick_l0(request, brick_id=None):
+    """
+    Top level rendering of a brick
+
+    :param request:
+    :param brick_id:
+    :return:
+    """
+    base_brick = get_object_or_404(Brick, pk=brick_id)
+
+    return render(request, 'sober/sober-main.html', {'brick': base_brick})
+
+
+def renderbrick_l1(request, brick):
+
+
+    return render(request, 'polls/detail.html', {'brick': brick})
