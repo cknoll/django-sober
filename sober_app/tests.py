daa13b245188c7e73ea04d05db89b77fce1ecbf5
Carsten Knoll <carsten.knoll@posteo.de>
2018-08-11 09:15:51 +0200

    first test case (with fixture for data)

--- sober_app/tests.py
+++ sober_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Brick
+
+# to get the current production data: ./manage.py dumpdata sober_app > sober_data.json
+
+
+class SoberModelTests1(TestCase):
+    fixtures = ['sober_data.json']
+
+    def test_find_some_childs(self):
+
+        try:
+            brick = Brick.objects.get(pk=1)
+        except Brick.DoesNotExist:
+            success = False
+        else:
+            success = True
+
+        self.assertTrue(success)
