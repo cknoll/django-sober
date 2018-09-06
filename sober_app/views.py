61b5bc2fb748401fe461158f92da1b46cada9c6f
Carsten Knoll <carsten.knoll@posteo.de>
2018-09-06 08:21:58 +0200

    wip: refactoring of data-structure for brick_tree

--- sober_app/views.py
+++ sober_app/views.py
@@ -57,8 +57,9 @@ def view_index(request):
     base_object.special_head_link = "new_thesis_link"
 
     for tbrick in thesis_list:
-        tbrick.template = "sober/{}".format(template_mapping.get(tbrick.type))
-        tbrick.title_tag = "Thesis#{}".format(tbrick.pk)
+        # trigger processing of the root of the respective trees (sufficient for the index)
+        BrickTree(tbrick, max_level=0)
+
     base_object.sorted_child_list = thesis_list
 
     return render(request, 'sober/main_brick_tree.html', {'base': base_object})
@@ -135,7 +136,7 @@ def view_new_brick(request, brick_id=None, type_code=None):
         sp.parent_brick = None
 
     else:
-        sp.parent_brick = prepare_base_brick_for_new_and_edit(brick_id)
+        sp.parent_brick = prepare_single_brick(brick_id)
 
     # here we process the submitted form
     if request.method == 'POST':
@@ -150,6 +151,7 @@ def view_new_brick(request, brick_id=None, type_code=None):
             new_brick.parent = sp.parent_brick
             new_brick.type = Brick.typecode_map[type_code]
             new_brick.save()
+            sp.newly_fabricated_brick = prepare_single_brick(new_brick.pk, add_level=1)
             sp.content = "no errors. Form saved. Result: {}".format(new_brick)
 
     # here we handle the generation of an empty form
@@ -159,7 +161,7 @@ def view_new_brick(request, brick_id=None, type_code=None):
         sp.form = brickform
 
     if hasattr(sp, "form"):
-        sp.form.form_type = "new"
+        # sp.form.form_type = "new"
 
         if type_code == Brick.reverse_typecode_map[Brick.thesis]:
             sp.form.action_url_name = "new_thesis"
@@ -173,7 +175,7 @@ def view_new_brick(request, brick_id=None, type_code=None):
 def view_edit_brick(request, brick_id=None):
     sp = Container()
 
-    sp.brick_to_edit = prepare_base_brick_for_new_and_edit(brick_id)
+    sp.brick_to_edit = prepare_single_brick(brick_id)
 
     lc = "en"
     type_code = Brick.reverse_typecode_map[sp.brick_to_edit.type]
@@ -192,6 +194,7 @@ def view_edit_brick(request, brick_id=None):
             edited_brick = brickform.save(commit=False)
             edited_brick.update_datetime = timezone.now()
             edited_brick.save()
+            sp.newly_fabricated_brick = prepare_single_brick(edited_brick.pk, add_level=1)
             sp.content = "no errors. Form saved. Result: {}".format(edited_brick)
 
     # here we handle the generation of an empty form
@@ -201,7 +204,7 @@ def view_edit_brick(request, brick_id=None):
         sp.form = brickform
 
     if hasattr(sp, "form"):
-        sp.form.form_type = "edit"
+        # sp.form.form_type = "edit"
         sp.form.action_url_name = "edit_brick"
 
     context = {"pagetype": "FORM-Mockup", "sp": sp, "brick_id": brick_id, "type_code": None}
@@ -214,23 +217,158 @@ def view_edit_brick(request, brick_id=None):
 # ------------------------------------------------------------------------
 
 
-def prepare_base_brick_for_new_and_edit(brick_id):
+class CounterWithAttributeAccess(collections.Counter):
+    """
+    This counter is able to be evaluated (for read-access) with {{cntr.pro}} or {{cntr.contra}} in the template
+    """
+    pass
+
+
+class BrickTree(object):
+    """
+    class representing a complete discussion
+    """
+
+    def __init__(self, brick, max_level=float("inf")):
+
+        self.root_parent, rp_level = get_root_parent(brick)
+        self.processed_bricks = collections.OrderedDict()
+        self._process_all_childs(self.root_parent, 0, max_level)
+
+    def _process_all_childs(self, brick, level, max_level=float("inf")):
+        """
+        for every node set the following attributes:
+         - absolute_level
+         - template
+         - title_tag
+         - parent_type_list
+
+        :return:   None
+        """
+
+        if level > max_level:
+            return
+
+        # if we already processed the brick then we take the respective object from our
+        # local store and not from database
+
+        brick = self.processed_bricks.get(brick.pk, brick)
+        self.processed_bricks[brick.pk] = brick
+
+        brick.absolute_level = level
+
+        type_counter = CounterWithAttributeAccess()
+        # iterate over all children to fix their chronological order (see use of typed_idx below)
+        for b in brick.children.all().order_by(*brick_ordering_chrono):
+            type_counter.update([b.type])
+
+            # dertermine that this child is e.g. the 3rd pro-brick on the current level
+            self.processed_bricks[b.pk] = b
+            b.typed_idx = type_counter[b.type]
+
+        # save the whole counter
+        brick.child_type_counter = type_counter
+
+        # allow simple access from the template:
+        for _, name, _ in Brick.type_names_codes:
+            name = name.lower()
+            key = getattr(Brick, name)
+
+            setattr(brick, "nbr_" + name, brick.child_type_counter[key])
+
+        brick.template = "sober/{}".format(template_mapping.get(brick.type))
+
+        self._set_title_tag(brick)
+
+        # now apply this method recursively to all child-bricks
+        # we save the query as attribute to avoid recomputation later
+        brick.direct_children = brick.children.all().order_by(*brick_ordering)
+
+        for i, b in enumerate(brick.direct_children):
+            self._process_all_childs(brick=b, level=level + 1)
+
+    def _set_title_tag(self, brick):
+        """
+        determine the title tag (something like pro#3⚡2⚡1?3) and the parent_type_list
+
+        :param brick:
+        :return: None (changes brick object)
+        """
+
+        if brick.parent is None:
+            assert brick.type == Brick.thesis
+            # other types must have a parent (!! what is with question?)
+
+            brick.parent_type_list = [(Brick.types_map[brick.type] + "#", brick.pk, brick.pk)]
+            # this is a list which stores tuples (split_symbol, xxx, pk) for each parent
+            # xxx is pk for thesis and typed_idx for other brick_types
+
+        else:
+            assert hasattr(brick.parent, "title_tag")
+            assert hasattr(brick.parent, "parent_type_list")
+            if not hasattr(brick, "typed_idx"):
+                IPS()
+            assert hasattr(brick, "typed_idx")
+
+            new_tuple = ( symbol_mapping[brick.type], brick.typed_idx, brick.pk )
+
+            brick.parent_type_list = brick.parent.parent_type_list + [new_tuple]
+
+        brick.title_tag = create_title_tag(brick.parent_type_list)
+
+    def get_subtree_as_list(self, base_brick, max_depth=float("inf")):
+        """
+        return a render-ready list of bricks
+
+        :param base_brick:
+        :param max_depth:           default: inf; 0 -> only the base_brick
+        :return:
+        """
+
+        res = [base_brick]
+        subtree_level = 0
+        bases = [base_brick]
+
+        while subtree_level < max_depth:
+            next_bases = []
+            for base in bases:
+                next_bases.extend(base.direct_children)
+                res.extend(base.direct_children)
+            subtree_level += 1
+            bases = next_bases
+
+        for brick in res:
+            brick.relative_level = brick.absolute_level - base_brick.absolute_level
+            if base_brick.type == Brick.thesis:
+                brick.indentation_class = "ml{}".format(max([0, brick.relative_level - 1]))
+            else:
+                brick.indentation_class = "ml{}".format(brick.relative_level)
+
+        return res
+
+
+def prepare_single_brick(brick_id, add_level=0):
 
     base_brick = get_object_or_404(Brick, pk=brick_id)
     root_parent, rp_level = get_root_parent(base_brick)
 
     # use this call to process all bricks down to the sibling level of base_brick
     # set template and relative links etc
+    print("pcb", brick_id, "add_level", add_level, "rpl", rp_level)
     process_child_bricks(root_parent,
                          root_type=root_parent.type,
                          current_level=0,
-                         rp_level=rp_level,
-                         max_level=rp_level)
+                         rp_level=0,
+                         max_level=rp_level,
+                         indentation_offset=rp_level-add_level)
+
+    print("--")
 
     return processed_bricks[base_brick.pk]
 
 
-def process_child_bricks(brick, root_type, rp_level, current_level, max_level):
+def process_child_bricks(brick, root_type, rp_level, current_level, max_level,
+                         indentation_offset=0):
     """
     This function will be called recursively.
     It sets the
@@ -243,9 +381,11 @@ def process_child_bricks(brick, root_type, rp_level, current_level, max_level):
     Return a flat list containing this brick and all childs up to max_level.
 
     :param brick:
-    :param root_type:       type of the root brick (used for some css selection)
-    :param current_level:
-    :param max_level:       int (this is for algorithmic safety.) See notes below
+    :param root_type:           type of the root brick (used for some css selection)
+    :param current_level:       level w.r.t. the tree of the processed brick
+    :param rp_level:            level distance between root_parent and shown base_brick
+    :param max_level:           int (this is for algorithmic safety.) See notes below
+    :param indentation_offset:  int (offset for the indentation-class)
 
     :return: list of bricks (up to max_level)
     """
@@ -254,9 +394,9 @@ def process_child_bricks(brick, root_type, rp_level, current_level, max_level):
     if current_level > max_level:
         return []
 
-
     brick.relative_level = current_level
     brick.absolute_level = current_level + rp_level
+    brick.indentation_offset = indentation_offset
 
     type_counter = collections.Counter()
     # iterate over all children to fix their chronological order (see the use of typed_idx below)
@@ -269,13 +409,16 @@ def process_child_bricks(brick, root_type, rp_level, current_level, max_level):
     # this enables us to display how much pro and contra args there are
     brick_attr_store[(brick.pk, "child_type_counter")] = type_counter
 
-    # Indentation (margin-left (ml)) should depend on the current (pseudo-)root
+    # Indentation (margin-left (ml)) should depend on the current base_brick
     # this logic could be implemented in the templates
     # but it would look ugly there (due to the lack of real variables)
+    # indentation_offset handles the case where the base_brick is not displayed
     if root_type == brick.thesis:
-        brick.indentation_class = "ml{}".format(max([0, current_level - 1]))
+        brick.indentation_class = "ml{}".format(max([0, current_level - 1 - indentation_offset]))
     else:
-        brick.indentation_class = "ml{}".format(current_level)
+        brick.indentation_class = "ml{}".format(current_level - indentation_offset)
+
+    print(brick, current_level, rp_level, indentation_offset, brick.indentation_class)
 
     # set the template
     brick.template = "sober/{}".format(template_mapping.get(brick.type))
@@ -289,7 +432,8 @@ def process_child_bricks(brick, root_type, rp_level, current_level, max_level):
 
     for i, b in enumerate(direct_children):
         res.extend(process_child_bricks(b, root_type, rp_level,
-                                        current_level + 1, max_level))
+                                        current_level + 1, max_level,
+                                        indentation_offset))
 
     return res
 
