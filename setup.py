16e5b8491b008b0a8b72f6a7852d0837a667de41
Carsten Knoll <carsten.knoll@posteo.de>
2019-12-27 12:24:01 +0100

    adapt license in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU Affero General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Framework :: Django',
         'Programming Language :: Python :: 3',
